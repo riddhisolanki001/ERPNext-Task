b680485444224c4379676d83f746a68ad5193799
riddhi_solanki <riddhssolanki048@gmail.com>
2022-09-13 17:07:50 +0530

    new doctype added

---
+++ myapp/myapp/doctype/hobbies/hobbies.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, riddhi and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Hobbies', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ myapp/myapp/doctype/registration/registration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, riddhi and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Registration', {
+	// refresh: function(frm) {
+
+	// }
+});
